1ede06b77c06c21f07a529d41e6241570397a04f
Adriane Maciel <adrianemaciel.developer@gmail.com>
2025-04-25 22:23:29 -0300

    feat: add WelcomeScreen and integrate it into HomeStack navigator

--- app/navigation/HomeStack.tsx
+++ app/navigation/HomeStack.tsx
@@ -1,12 +1,21 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import AddTripScreen from "../screens/AddTripScreen";
+import WelcomeScreen from "../screens/WelcomeScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function HomeStack() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      initialRouteName="Welcome"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen
+        name="Welcome"
+        component={WelcomeScreen}
+        options={{ title: "Bem-vindo" }}
+      />
       <Stack.Screen
         name="Home"
         component={HomeScreen}
